89e4db91e8078c430f7608288422ee1a6bf83180
Oliwia <oliwiaskrgit@gmail.com>
2024-12-22 19:14:18 +0100

    hotfix- landing page

--- backend/emergencyBackend/emergency_app/views.py
+++ backend/emergencyBackend/emergency_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-
+@api_view(['GET'])
 def landing_page(request):
     return Response({
         'message': 'Hello from the backend!'
