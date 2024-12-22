d2adf41b3d8724489c00e10a4fe8e32017c00880
Oliwia <oliwiaskrgit@gmail.com>
2024-12-22 18:43:38 +0100

    feature_landingpage_backend- urls set up, django rest framework added

--- backend/emergencyBackend/emergency_app/urls.py
+++ backend/emergencyBackend/emergency_app/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns= [
-
+    path('', views.landing_page, name='landing_page'),
 ]
 
 if settings.DEBUG:
--- backend/emergencyBackend/emergency_app/views.py
+++ backend/emergencyBackend/emergency_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+def landing_page(request):
+    return Response({
+        'message': 'Hello from the backend!'
+    }, status=200)
+
+
+#landing page- when we have auth setup
+# @api_view(['GET'])
+# def landing_page(request):
+#     login_url = request.build_absolute_uri(reverse('login_user'))
+#     return Response({
+#         'message': 'Redirection succeeded',
+#         'login_url': login_url,
+#     }, status=200)
