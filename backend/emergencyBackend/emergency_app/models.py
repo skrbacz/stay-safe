78b060b78aa395451176bc6ceb9624f14696678d
Oliwia <oliwiaskrgit@gmail.com>
2024-12-22 19:10:46 +0100

    importand change in natural disaster model

--- backend/emergencyBackend/emergency_app/models.py
+++ backend/emergencyBackend/emergency_app/models.py
@@ -22,7 +22,7 @@ class CityModel(models.Model):
 
 class NaturalDisasterModel(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=1000)
     todo_list = models.CharField(max_length=1000)
 
