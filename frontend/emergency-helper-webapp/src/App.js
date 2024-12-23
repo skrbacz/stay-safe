aeb977be65806497bf7a5e529d9297b2c72f2a40
Oliwia <oliwiaskrgit@gmail.com>
2024-12-23 16:55:36 +0100

    frontend in .js

---
+++ frontend/emergency-helper-webapp/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+     
+      </Routes>
+    </Router>
+  );
+};
+export default App;
