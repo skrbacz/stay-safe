ee931716e53107d31ea2c5298b51b2e57ef53b36
Oliwia <oliwiaskrgit@gmail.com>
2025-01-14 18:26:04 +0100

    develop- updated models

--- backend/emergencyBackend/emergency_app/models.py
+++ backend/emergencyBackend/emergency_app/models.py
@@ -2,22 +2,21 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
-class CityModel(models.Model):
+class District(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    postal_code = models.IntegerField()
 
-    CITY_STATES = [
+    DISTRICT_STATES = [
         ('safe', 'Safe'),
         ('natural_disaster_incoming', 'Natural disaster incoming'),
         ('natural_disaster_ongoing', 'Natural disaster ongoing'),
         ('natural_disaster_ended', 'Natural disaster ended'),
     ]
-    state = models.CharField(max_length=100, choices=CITY_STATES, default='safe')
+    state = models.CharField(max_length=100, choices=DISTRICT_STATES, default='safe')
 
     def __str__(self):
-        return self.name, self.country, self.postal_code, "state:", self.state
+        return self.name, self.country, "state:", self.state
 
 
 class NaturalDisasterModel(models.Model):
@@ -30,15 +29,23 @@ class NaturalDisasterModel(models.Model):
         return self.name
 
 
-class NaturalDisasterInCityModel(models.Model):
+class NaturalDisasterInDistrict(models.Model):
     id = models.AutoField(primary_key=True)
     natural_disaster = models.ForeignKey(NaturalDisasterModel, on_delete=models.CASCADE)
-    city = models.ForeignKey(CityModel, on_delete=models.CASCADE)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return ("disaster name:", self.natural_disaster.name, "city name:", self.city.name,
-                "city state:", self.city.state, self.date)
+        return ("disaster name:", self.natural_disaster.name, "district name:", self.district.name,
+                "city state:", self.district.state, self.date)
+
+
+class DistrictCode(models.Model):
+    district_code = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 
 
 class UserManager(BaseUserManager):
@@ -61,11 +68,11 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100)
     hashed_password = models.CharField(max_length=255)
-    city = models.ForeignKey(CityModel, null= True, blank=True, on_delete=models.SET_NULL)
+    district = models.ForeignKey(District, null=True, blank=True, on_delete=models.SET_NULL)
 
-    objects= UserManager()
+    objects = UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return self.email
+        return self.emailt
