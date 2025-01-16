0c68fb4dbff28b655512b7e01d75bbf774e23dc3
Oliwia <oliwiaskrgit@gmail.com>
2025-01-16 14:47:54 +0100

    working post for nd

--- backend/emergencyBackend/emergency_app/urls.py
+++ backend/emergencyBackend/emergency_app/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     # path('check_login/', views.check_login, name='check_login'),
     path('', views.landing_page, name='landing_page'),
     path('natural_disaster/<str:name>/', views.get_natural_disaster_by_name, name='natural_disaster_one'),
-    path('natural_disaster/', views.get_all_natural_disasters, name='natural_disaster_list'),
-    path('natural_disaster/create/', views.post_natural_disaster, name='post_natural_disaster'),
+    path('natural_disaster/', views.get_all_natural_disasters, name='get_all_natural_disasters'),
+    path('natural_disaster/create', views.post_natural_disaster, name='post_natural_disaster'),
     path('natural_disaster/delete/<str:name>/', views.delete_natural_disaster, name='delete_natural_disaster'),
 
 ]
