b4ebff3d1cb30a7a13c03f9393272e6c6c42ba1a
Oliwia <oliwiaskrgit@gmail.com>
2025-01-16 13:53:22 +0100

    feature_login_frontend- working login page

--- frontend/emergency-helper-webapp/src/App.js
+++ frontend/emergency-helper-webapp/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import Login from "./login_page/Login";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-     
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
