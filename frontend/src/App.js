1406cf291371d218fdb56d1358737f0aa3e08cc3
Oliwia <oliwiaskrgit@gmail.com>
2025-01-21 14:42:48 +0100

    merged disaster page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./login_page/Login";
 import Landing from "./landing_page/landing.js";
 import HomePage from "./home_page/Home.js";
+import Disaster from "./disaster_page/Disaster.js";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<HomePage/>}/>
+        <Route path= "/disaster" element={<Disaster/>}/>
       </Routes>
     </Router>
   );
